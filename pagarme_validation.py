2903091fe8d6d382d05cb6f06436322964389a41
matheus-mac <matheus.silva@escoladnc.com.br>
2024-03-11 08:20:33 -0300

    mmiddle of analysis

--- pagarme_validation.py
+++ pagarme_validation.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 import os
 import pandas as pd
 
@@ -59,6 +60,8 @@ def local_df_load_extrato_diario():
             if file.endswith('.csv'):
                 file_path = os.path.join(subdir, file)
                 df = pd.read_csv(file_path)
+                # Make all column names lowercase
+                df.columns = df.columns.str.lower()
                 dfs.append(df)
     # Concatenate DataFrames vertically
     combined_df = pd.concat(dfs, ignore_index=True)
@@ -67,9 +70,10 @@ def local_df_load_extrato_diario():
 
 def adjust_sales(df_sales):
     # select only data between desired time
-    df_sales['data_caixa'] = pd.to_datetime(df_sales['recebimento_financiamento']).dt.normalize()
-    df_sales = df_sales[(df_sales['data_caixa'] >= pd.to_datetime('2023-01-01')) &
-                        (df_sales['data_caixa'] < pd.to_datetime('2024-01-01'))]
+    df_sales['venda_caixa'] = pd.to_datetime(df_sales['recebimento_financiamento']).dt.normalize()
+    df_sales['refund_caixa'] = pd.to_datetime(df_sales['efetivacao_cancelamento']).dt.normalize()
+    df_sales = df_sales[(df_sales['venda_caixa'] >= pd.to_datetime('2023-01-01')) &
+                        (df_sales['venda_caixa'] < pd.to_datetime('2024-01-01'))]
     # create id and installment from gateway_id
     df_sales['parcela'] = df_sales['gateway_id'].str.split("-").str[0]
     df_sales['parcela'].replace(r'\b0\b', '1', regex=True, inplace=True)
@@ -77,22 +81,47 @@ def adjust_sales(df_sales):
     # define types
     df_sales[['parcela', 'transaction_id']] = df_sales[['parcela', 'transaction_id']].astype(str)
     df_sales.fillna('0', inplace=True)
-    df_sales[['valor_total_venda', 'valor_taxa_total', 'valor_cancelamento', 'reembolso_taxa']] = \
+    df_sales[['venda', 'venda_taxa', 'refund', 'reembolso_taxa']] = \
         df_sales[['valor_total_venda', 'valor_taxa_total', 'valor_cancelamento', 'reembolso_taxa']].astype(float)
     # restrict columns to analyze
-    colums_to_keep = ['gateway_id', 'data_venda', 'valor_total_venda', 'valor_taxa_total', 'valor_cancelamento',
-                      'reembolso_taxa', 'transaction_id', 'parcela', 'data_caixa']
+    colums_to_keep = ['gateway_id', 'data_venda', 'venda', 'venda_taxa', 'refund',
+                      'reembolso_taxa', 'transaction_id', 'parcela', 'venda_caixa', 'refund_caixa']
     return df_sales[colums_to_keep]
 
 
 def adjust_extrato():
     df_extrato = local_df_load_extrato_diario()
-    df_extrato['data_caixa'] = pd.to_datetime(df_extrato['Data de pagamento'], format='%d/%m/%Y %H:%M').dt.normalize()
-    df_extrato.rename(columns={'ID da Transação': 'nsu', 'Parcela': 'parcela'}, inplace=True)
+    df_extrato['venda_caixa'] = pd.to_datetime(df_extrato['data de pagamento'], format='%d/%m/%Y %H:%M').dt.normalize()
+    df_extrato.rename(columns={'id da transação': 'nsu', 'parcela': 'parcela', 'data de criação': 'data_venda'},
+                      inplace=True)
     # adjust types
     df_extrato['parcela'].replace({'-': '1'}, inplace=True)
     df_extrato[['nsu', 'parcela']] = df_extrato[['nsu', 'parcela']].astype(str)
-    return df_extrato
+    # Create comparable columns to sales
+    df_extrato.replace({'-': '0'}, inplace=True)
+    df_extrato['nsu'] = df_extrato['nsu'].astype(str)
+    df_extrato['nsu'] = df_extrato['nsu'].str.split(".").str[0]
+    df_extrato = df_extrato.applymap(lambda x: x.replace('.', '') if isinstance(x, str) else x)
+    df_extrato = df_extrato.applymap(lambda x: x.replace(',', '.') if isinstance(x, str) else x)
+    df_extrato['venda'] = df_extrato['entrada'].astype(float)
+    df_extrato['refund'] = df_extrato['saída'].astype(float)
+    df_extrato['taxa total da operação'] = df_extrato['taxa total da operação'].astype(float)
+    df_extrato['venda_taxa'] = np.where(df_extrato['venda'] > 0, df_extrato['taxa total da operação'], 0)
+    df_extrato['reembolso_taxa'] = np.where(df_extrato['refund'] < 0, df_extrato['taxa total da operação'], 0)
+    # Create venda_caixa for venda and refund
+    df_extrato['refund_caixa'] = df_extrato['venda_caixa']
+    df_extrato.loc[df_extrato['venda'] > 0, 'refund_caixa'] = pd.to_datetime('1987-12-17')  # no value
+    df_extrato.loc[df_extrato['refund'] < 0, 'venda_caixa'] = pd.to_datetime('1987-12-17')  # no value
+    # restrict columns to analyze
+    colums_to_keep = ['data_venda', 'venda', 'venda_taxa', 'refund',
+                      'reembolso_taxa', 'nsu', 'parcela', 'venda_caixa', 'refund_caixa']
+    df_extrato = df_extrato[colums_to_keep]
+    # Group transactions and installments from credit and refund
+    df_extrato = df_extrato.groupby(['nsu', 'parcela']).\
+        agg({'data_venda': 'max', 'venda': 'sum', 'venda_taxa': 'sum', 'refund': 'sum', 'reembolso_taxa': 'sum',
+             'venda_caixa': 'max', 'refund_caixa': 'max'
+             }).reset_index()
+    return df_extrato[colums_to_keep]
 
 
 def adjust_transactions():
@@ -112,13 +141,13 @@ def extrato_check(df_sales):
 
     df_extrato = df_extrato.merge(df_transactions, on=['nsu'], how='left')
 
-    df_compare = df_sales.merge(df_extrato, on=['transaction_id', 'parcela'], how='left')
+    df_compare = df_sales.merge(df_extrato, on=['transaction_id', 'parcela'], suffixes=('_sales', '_extrato'),
+                                how='left', indicator=True)
 
-    df_group_sum = df_sales.groupby(pd.Grouper(key='data_caixa', freq='ME')).agg(
-        {'valor_total_venda': 'sum', 'valor_taxa_total': 'sum', 'valor_cancelamento': 'sum', 'reembolso_taxa': 'sum',
-         'juros_atraso': 'sum'})
-    df_group_sum_interest = df_group_sum[df_group_sum.index > pd.to_datetime('2023-01-01')]
-    df_group_sum_interest['total'] = df_group_sum_interest.sum(axis=1)
+    df_jan = df_compare[(df_compare['venda_caixa_sales'] >= pd.to_datetime('2023-01-01')) &
+                        (df_compare['venda_caixa_sales'] < pd.to_datetime('2023-02-01'))]
+    df_jan.loc[df_jan['refund_caixa_extrato'] == pd.to_datetime('1987-12-17'), 'refund_caixa_extrato'] = 0
+
     pass
 
 
